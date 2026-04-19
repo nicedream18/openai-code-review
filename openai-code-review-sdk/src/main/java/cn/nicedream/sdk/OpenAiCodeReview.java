99a6b31bd0692bd77e6c10fc3cc9022af5c21db4
nicedream <nicedream@example.com>
2026-04-19 22:38:40 +0800

    feat:代码评审 v4.1

--- openai-code-review-sdk/src/main/java/cn/nicedream/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/cn/nicedream/sdk/OpenAiCodeReview.java
@@ -1,6 +1,8 @@
 package cn.nicedream.sdk;
 
+import cn.nicedream.sdk.domain.model.ChatCompletionRequest;
 import cn.nicedream.sdk.domain.model.ChatCompletionSyncResponse;
+import cn.nicedream.sdk.domain.model.Model;
 import cn.nicedream.sdk.type.utils.BearerTokenUtils;
 import com.alibaba.fastjson2.JSON;
 
@@ -8,6 +10,7 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
 public class OpenAiCodeReview {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -48,18 +51,28 @@ public class OpenAiCodeReview {
         connection.setDoOutput(true);
 
 
-        String jsonInpuString = "{"
-                + "\"model\":\"glm-4-flash\","
-                + "\"messages\": ["
-                + "    {"
-                + "        \"role\": \"user\","
-                + "        \"content\": \"你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码为: " + diffCode + "\""
-                + "    }"
-                + "]"
-                + "}";
+//        String jsonInpuString = "{"
+//                + "\"model\":\"glm-4-flash\","
+//                + "\"messages\": ["
+//                + "    {"
+//                + "        \"role\": \"user\","
+//                + "        \"content\": \"你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码为: " + diffCode + "\""
+//                + "    }"
+//                + "]"
+//                + "}";
 
+        ChatCompletionRequest chatCompletionRequest = new ChatCompletionRequest();
+        chatCompletionRequest.setModel(Model.GLM_4_FLASH.getCode());
+        chatCompletionRequest.setMessages(new ArrayList<ChatCompletionRequest.Prompt>() {
+            private static final long serialVersionUID = -7988151926241837899L;
+
+            {
+                add(new ChatCompletionRequest.Prompt("user", "你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码如下:"));
+                add(new ChatCompletionRequest.Prompt("user", diffCode));
+            }
+        });
         try(OutputStream os = connection.getOutputStream()){
-            byte[] input = jsonInpuString.getBytes(StandardCharsets.UTF_8);
+            byte[] input = JSON.toJSONString(chatCompletionRequest).getBytes(StandardCharsets.UTF_8);
             os.write(input);
         }
 
@@ -77,6 +90,7 @@ public class OpenAiCodeReview {
         in.close();
         connection.disconnect();
 
+        System.out.println("评审结果："+content.toString());
         ChatCompletionSyncResponse response = JSON.parseObject(content.toString(), ChatCompletionSyncResponse.class);
         return response.getChoices().get(0).getMessage().getContent();
     }
